60f9830408b298e11fd02efcb406fb3ec7948b91
massnomis <massnomis@gmail.com>
2023-05-23 11:14:21 +0300

    a

--- smolcam.py
+++ smolcam.py
@@ -1,11 +1,12 @@
 from streamlit_webrtc import webrtc_streamer
 import av
-
-
+import streamlit as st
+net_placeholder = st.empty()
 def video_frame_callback(frame):
     img = frame.to_ndarray(format="bgr24")
 
     flipped = img[::-1,:,:]
+    net_placeholder.write(flipped, channels="BGR")
 
     return av.VideoFrame.from_ndarray(flipped, format="bgr24")
 
